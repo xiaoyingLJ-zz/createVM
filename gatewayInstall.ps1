bf8db258b17a4dcf17244967aae4a32873909faf
xiaoyingLJ <xiaoyz@microsoft.com>
2016-12-21 11:12:25 +0800

    add log

--- gatewayInstall.ps1
+++ gatewayInstall.ps1
@@ -5,19 +5,65 @@
  $vmdnsname
 )
 
-
-$uri = "https://wu.configuration.dataproxy.clouddatahub.net/GatewayClient/GatewayBits?version={0}&language={1}&platform={2}" -f "latest","en-US","x64"
-$gwPath= $env:SystemRoot + "\gateway.msi"
-$client = New-Object System.Net.WebClient
-$gatewayInfo = $client.DownloadString($uri) | ConvertTo-Json  | ConvertFrom-Json
-$psobject = $gatewayInfo | ConvertFrom-Json
-$downloadPath = $psobject | select -ExpandProperty "gatewayBitsLink"
-$hashValue = $psobject | select -ExpandProperty "gatewayBitHash"
-$client.DownloadFile($downloadPath, $gwPath)
-
-function DMDTTP-RunProcess([string] $process, [string] $arguments)
+$logLoc = "$env:SystemDrive\WindowsAzure\Logs\Plugins\Microsoft.Compute.CustomScriptExtension\"
+if (! (Test-Path($logLoc)))
 {
-	Write-Verbose "DMDTTP-RunProcess: $process $arguments"
+    Trace-Log "Can't find the log folder $logLoc, will create new directory"
+    New-Item -path $logLoc -type directory -Force
+    Trace-Log "Created the log folder $logLoc"
+}
+$logPath = "$logLoc\tracelog.log"
+Trace-Log "Log file: $logLoc"
+$uri = "https://wu.configuration.dataproxy.clouddatahub.net/GatewayClient/GatewayBits?version={0}&language={1}&platform={2}" -f "latest","en-US","x64"
+Trace-Log "Configuration service url: $uri"
+$gwPath= "$env:tmp\gateway.msi"
+Trace-Log "Gateway download location: $gwPath"
+
+Download-Gateway $uri $gwPath
+Install-Gateway $gwPath
+Register-Gateway $gatewayKey
+
+$regkey = "hklm:\Software\Microsoft\DataTransfer\DataManagementGateway\HostService"
+Set-ItemProperty -Path $regkey -Name ExternalHostName -Value $vmdnsname
+
+
+function Now-Value()
+{
+    return (Get-Date -Format "yyyy-MM-dd HH:mm:ss")
+}
+
+function Throw-Error([string] $msg)
+{
+	try 
+	{
+		throw $msg
+	} 
+	catch 
+	{
+		$stack = $_.ScriptStackTrace
+		Trace-Log "DMDTTP is failed: $msg`nStack:`n$stack"
+	}
+
+	throw $msg
+}
+
+function Trace-Log([string] $msg)
+{
+    $now = Now-Value
+    try
+    {
+        "${now} $msg`n" | Out-File $logPath -Append
+    }
+    catch
+    {
+        #ignore any exception during trace
+    }
+
+}
+
+function Run-Process([string] $process, [string] $arguments)
+{
+	Write-Verbose "Run-Process: $process $arguments"
 	
 	$errorFile = "$env:tmp\tmp$pid.err"
 	$outFile = "$env:tmp\tmp$pid.out"
@@ -45,10 +91,10 @@ function DMDTTP-RunProcess([string] $process, [string] $arguments)
 
 	if($proc.ExitCode -ne 0 -or $errVariable -ne "")
 	{		
-		throw "Failed to run process: exitCode=$($proc.ExitCode), errVariable=$errVariable, errContent=$errContent, outContent=$outContent."
+		Throw-Error "Failed to run process: exitCode=$($proc.ExitCode), errVariable=$errVariable, errContent=$errContent, outContent=$outContent."
 	}
 
-	Write-Verbose "DMDTTP-RunProcess: ExitCode=$($proc.ExitCode), output=$outContent"
+	Trace-Log "Run-Process: ExitCode=$($proc.ExitCode), output=$outContent"
 
 	if ([string]::IsNullOrEmpty($outContent))
 	{
@@ -58,11 +104,46 @@ function DMDTTP-RunProcess([string] $process, [string] $arguments)
 	return $outContent.Trim()
 }
 
+function Download-Gateway([string] $url, [string] $gwPath)
+{
+    try
+    {
+        $ErrorActionPreference = "Stop";
+        $client = New-Object System.Net.WebClient
+        $gatewayInfo = $client.DownloadString($uri)
+        Trace-Log "Get gateway information successfully. $gatewayInfo"
+        $psobject = $gatewayInfo | ConvertFrom-Json
+        $downloadPath = $psobject | select -ExpandProperty "gatewayBitsLink"
+        Trace-Log "Gateway download path: $downloadPath"
+        $hashValue = $psobject | select -ExpandProperty "gatewayBitHash"
+        Trace-Log "Expected gateway bit hash value: $hashValue"
+        $client.DownloadFile($downloadPath, $gwPath)
+        Trace-Log "Download gateway successfully. Gateway loc: $gwPath"
+    }
+    catch
+    {
+        Trace-Log "Fail to download gateway msi"
+        Trace-Log $_.Exception.ToString()
+        throw
+    }
+}
+
 function Verify-Signature([string] $gwPath, [string] $hashValue)
 {
+    Trace-Log "Begin to verify gateway signature."
+    if ([string]::IsNullOrEmpty($gwPath))
+    {
+		throw "Gateway path is not specified"
+    }
+
+	if (!(Test-Path -Path $gwPath))
+	{
+		throw "Invalid gateway path: $gwPath"
+	}
     $hasher = [System.Security.Cryptography.SHA256CryptoServiceProvider]::Create()
     $content = [System.IO.File]::OpenRead($gwPath)
     $hash = [System.Convert]::ToBase64String($hasher.ComputeHash($content))
+    Trace-Log "Real gateway hash value: $hash"
     return ($hash -eq $hashValue)
 }
 
@@ -83,28 +164,30 @@ function Install-Gateway([string] $gwPath)
         throw "invalid gateway msi"
     }
 	
-	Write-Verbose "Copy Gateway installer from $gwPath to current location: $PWD"	
+	Trace-Log "Copy Gateway installer from $gwPath to current location: $PWD"	
 	Copy-Item -Path $gwPath -Destination $PWD -Force
-	Write-Verbose "Start Gateway installation"
-	DMDTTP-RunProcess "msiexec.exe" "/i gateway.msi /quiet /passive"		
+	Trace-Log "Start Gateway installation"
+	Run-Process "msiexec.exe" "/i gateway.msi /quiet /passive"		
 	
 	Start-Sleep -Seconds 30	
 
-	Write-Verbose "Installation of gateway is successful"
+	Trace-Log "Installation of gateway is successful"
+
+    Remove-Item $gwPath
 }
 
-function GetRegistryProperty([string] $keyPath, [string] $property)
+function Get-RegistryProperty([string] $keyPath, [string] $property)
 {
-	Write-Verbose "GetRegistryProperty: Get $property from $keyPath"
+	Trace-Log "Get-RegistryProperty: Get $property from $keyPath"
 	if (! (Test-Path $keyPath))
 	{
-		Write-Verbose "GetRegistryProperty: $keyPath does not exist"
+		Trace-Log "Get-RegistryProperty: $keyPath does not exist"
 	}
 
 	$keyReg = Get-Item $keyPath
 	if (! ($keyReg.Property -contains $property))
 	{
-		Write-Verbose "GetRegistryProperty: $property does not exist"
+		Trace-Log "Get-RegistryProperty: $property does not exist"
 		return ""
 	}
 
@@ -113,10 +196,10 @@ function GetRegistryProperty([string] $keyPath, [string] $property)
 
 function Get-InstalledFilePath()
 {
-	$filePath = GetRegistryProperty "hklm:\Software\Microsoft\DataTransfer\DataManagementGateway\ConfigurationManager" "DiacmdPath"
+	$filePath = Get-RegistryProperty "hklm:\Software\Microsoft\DataTransfer\DataManagementGateway\ConfigurationManager" "DiacmdPath"
 	if ([string]::IsNullOrEmpty($filePath))
 	{
-		throw "Get-InstalledFilePath: Cannot find installed File Path"
+		Throw-Error "Get-InstalledFilePath: Cannot find installed File Path"
 	}
 
 	return $filePath
@@ -124,15 +207,11 @@ function Get-InstalledFilePath()
 
 function Register-Gateway([string] $instanceKey)
 {
-    Write-Verbose "Register Agent"
+    Trace-Log "Register Agent"
 	$filePath = Get-InstalledFilePath
-	DMDTTP-RunProcess $filePath "-k $instanceKey"
+	Run-Process $filePath "-k $instanceKey"
 
 	Restart-Service DIAHostService -WarningAction:SilentlyContinue
-    Write-Verbose "Agent registration is successful!"
+    Trace-Log "Agent registration is successful!"
 }
-Install-Gateway $gwPath
-Register-Gateway $gatewayKey
 
-$regkey = "hklm:\Software\Microsoft\DataTransfer\DataManagementGateway\HostService"
-Set-ItemProperty -Path $regkey -Name ExternalHostName -Value $vmdnsname
